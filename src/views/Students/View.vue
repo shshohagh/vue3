9950ea04e19f6ad8f20a41825e8de859d6465e42
Sohag <80314649+shshohagh@users.noreply.github.com>
2024-06-18 20:44:24 +0600

    add axios

--- src/views/Students/View.vue
+++ src/views/Students/View.vue
@@ -1,5 +1,58 @@
 <template>
-    <main>
-        <h1>This is Students page</h1>
-    </main>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h4>
+                    Students
+                    <RouterLink to="/students" class="btn btn-primary float-end">Add Student</RouterLink>
+                </h4>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(user, index) in users" :key="index">
+                            <td>{{ user.id }}</td>
+                            <td>{{ user.name }}</td>
+                            <td>{{ user.email }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'Users',
+    data(){
+        return {
+            users: []
+        }
+    },
+    mounted() {
+        this.getUsers();
+    },
+    methods: {
+        getUsers(){
+
+            axios.get('http://127.0.0.1:8000/api/users')
+            .then(res => {
+                this.users = res.data.data;
+            })
+            .catch(error => {
+                    console.log('Error fetching users:', error);
+            });
+        }
+    },
+}
+
+</script>
